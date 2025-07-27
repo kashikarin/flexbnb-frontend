c60e45bf176cbd29615e06ff1baa97f742b1854b
Idan Bahar <bahar.idan91@gmail.com>
2025-07-27 23:39:28 +0300

    some styling changes

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -6,11 +6,6 @@ import { loadUser } from '../store/user.actions'
 import { store } from '../store/store'
 import { showSuccessMsg } from '../services/event-bus.service'
 import {
-  socketService,
-  SOCKET_EVENT_USER_UPDATED,
-  SOCKET_EMIT_USER_WATCH,
-} from '../services/socket.service'
-import {
   loadOrder,
   loadOrders,
   updateOrderStatus,
@@ -28,20 +23,7 @@ export function UserDetails() {
   useEffect(() => {
     loadUser(params.id)
     loadOrders({})
-    socketService.emit(SOCKET_EMIT_USER_WATCH, params.id)
-    socketService.on(SOCKET_EVENT_USER_UPDATED, onUserUpdate)
-
-    return () => {
-      socketService.off(SOCKET_EVENT_USER_UPDATED, onUserUpdate)
-    }
   }, [params.id])
-
-  function onUserUpdate(user) {
-    showSuccessMsg(
-      `This user ${user.fullname} just got updated from socket, new score: ${user.score}`
-    )
-    store.dispatch({ type: 'SET_WATCHED_USER', user })
-  }
 
   return (
     <main className='user-details'>

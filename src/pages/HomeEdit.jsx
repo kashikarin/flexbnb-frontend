329d510b8c6edbbae9e7e443c30a0731398f2fb1
kashikarin <karinkashi@gmail.com>
2025-07-29 01:19:30 +0300

    add hosting pages and styling

---
+++ src/pages/HomeEdit.jsx
@@ -0,0 +1,18 @@
+export function HomeEdit(){
+    return(
+        <section className="home-edit-container">
+            <div className="home-edit-form">
+                <div className="home-edit-step-1 selected">
+                    <div className="home-edit-step-1-text-container">
+                        <span>Step 1</span>
+                        <span>Tell us about your place</span>
+                        <span>In this step, we'll ask you which type of property you have and if guests will book the entire place or just a room. Then let us know the location and how many guests can stay</span>
+                    </div>
+                    <div className="home-edit-step-1-video-container">
+                        <video src="https://stream.media.muscache.com/zFaydEaihX6LP01x8TSCl76WHblb01Z01RrFELxyCXoNek.mp4?v_q=high" autoplay="" playsinline="" class="video" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}

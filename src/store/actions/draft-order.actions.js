6319b019e5a7b61c7c389c05e022b7d382ee3309
Idan Bahar <bahar.idan91@gmail.com>
2025-09-14 22:51:40 +0300

    disable the option to reserve home when unsignned user

--- src/store/actions/draft-order.actions.js
+++ src/store/actions/draft-order.actions.js
@@ -1,38 +1,43 @@
-import { draftOrderService } from "../../services/draft-order/draft-order.service.local";
-import { ADD_DRAFT_ORDER, 
-         CLEAR_DRAFT_ORDER, 
-         CLOSE_ORDER_CONFIRMATION_MODAL, 
-         OPEN_ORDER_CONFIRMATION_MODAL, 
-         UPDATE_DRAFT_ORDER 
-    } from "../reducers/draft-order.reducer";
+import { draftOrderService } from '../../services/draft-order/draft-order.service.local'
+import {
+  ADD_DRAFT_ORDER,
+  CLEAR_DRAFT_ORDER,
+  CLOSE_ORDER_CONFIRMATION_MODAL,
+  OPEN_ORDER_CONFIRMATION_MODAL,
+  UPDATE_DRAFT_ORDER,
+} from '../reducers/draft-order.reducer'
 import { store } from '../store'
 
 export async function addDraftOrder(homeId, userId, filterBy) {
-    try {
-        const draftOrder = await draftOrderService.getDraftOrder(homeId, userId, filterBy)
-        store.dispatch(getCmdAddDraftOrder(draftOrder))
-        return draftOrder
-    } catch(err){
-        console.error('Cannot add draft order', err)
-        throw err
-    }
+  try {
+    const draftOrder = await draftOrderService.getDraftOrder(
+      homeId,
+      userId,
+      filterBy
+    )
+    store.dispatch(getCmdAddDraftOrder(draftOrder))
+    return draftOrder
+  } catch (err) {
+    console.error('Cannot add draft order', err)
+    throw err
+  }
 }
 
-export  function removeDraftOrder() {
-    store.dispatch(getCmdClearDraftOrder())
+export function removeDraftOrder() {
+  store.dispatch(getCmdClearDraftOrder())
 }
 
 export function updateDraftOrder(draftOrder) {
-    store.dispatch(getCmdUpdateDraftOrder(draftOrder))
-    return draftOrder
+  store.dispatch(getCmdUpdateDraftOrder(draftOrder))
+  return draftOrder
 }
 
-export function closeOrderConfirmationModal(){
-    store.dispatch(getCmdCloseOrderConfirmationModal())
+export function closeOrderConfirmationModal() {
+  store.dispatch(getCmdCloseOrderConfirmationModal())
 }
 
-export function openOrderConfirmationModal(){
-    store.dispatch(getCmdOpenOrderConfirmationModal())
+export function openOrderConfirmationModal() {
+  store.dispatch(getCmdOpenOrderConfirmationModal())
 }
 
 // Command Creators:
@@ -40,30 +45,30 @@ export function openOrderConfirmationModal(){
 function getCmdAddDraftOrder(draftOrder) {
   return {
     type: ADD_DRAFT_ORDER,
-    draftOrder
+    draftOrder,
   }
 }
 
 function getCmdClearDraftOrder() {
   return {
-    type: CLEAR_DRAFT_ORDER
+    type: CLEAR_DRAFT_ORDER,
   }
 }
 function getCmdUpdateDraftOrder(draftOrder) {
   return {
     type: UPDATE_DRAFT_ORDER,
-    draftOrder
+    draftOrder,
   }
 }
 
-function getCmdCloseOrderConfirmationModal(){
-    return {
-        type: CLOSE_ORDER_CONFIRMATION_MODAL
-    }
+function getCmdCloseOrderConfirmationModal() {
+  return {
+    type: CLOSE_ORDER_CONFIRMATION_MODAL,
+  }
 }
 
-function getCmdOpenOrderConfirmationModal(){
-    return {
-        type: OPEN_ORDER_CONFIRMATION_MODAL
-    }
+function getCmdOpenOrderConfirmationModal() {
+  return {
+    type: OPEN_ORDER_CONFIRMATION_MODAL,
+  }
 }

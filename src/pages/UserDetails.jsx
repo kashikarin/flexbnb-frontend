3d76fc75f1c6fdc3ad6e60473f65e5d82a0e5213
Idan Bahar <bahar.idan91@gmail.com>
2025-09-15 22:44:21 +0300

    fixing user details

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -18,34 +18,33 @@ import { draftOrderService } from '../services/draft-order/draft-order.service.l
 
 export function UserDetails() {
   const params = useParams()
-  const {getNumberOfNights} = draftOrderService
+  const { getNumberOfNights } = draftOrderService
   // const user = useSelector((storeState) => storeState.userModule.watchedUser)
   const orders = useSelector((storeState) => storeState.orderModule.orders)
-  console.log("🚀 ~ orders:", orders)
-  
+  console.log('🚀 ~ orders:', orders)
+
   useEffect(() => {
     loadUser(params.id)
     loadOrders({})
   }, [params.id])
 
-  async function onUpdateOrderStatus(order, updatedStatus){
-    const orderToUpdate = { ...order, status: updatedStatus}
+  async function onUpdateOrderStatus(order, updatedStatus) {
+    const orderToUpdate = { ...order, status: updatedStatus }
     try {
       await updateOrder(orderToUpdate)
-    } catch(err){
+    } catch (err) {
       console.error('Cannot update order status', err)
     }
   }
 
-  function getPricePerNight(totalPrice, nightsNum){
-    return Math.round(totalPrice / (nightsNum * (1 + 0.14)))    
+  function getPricePerNight(totalPrice, nightsNum) {
+    return Math.round(totalPrice / (nightsNum * (1 + 0.14)))
   }
 
-  
   return (
-    <main className='user-details'>
+    <main className="user-details">
       <section
-        className='charts'
+        className="charts"
         style={{
           display: 'flex',
           gap: '20px',
@@ -54,42 +53,45 @@ export function UserDetails() {
           justifyContent: 'space-evenly',
         }}
       >
-        <div className='chart-container'>
+        <div className="chart-container">
           <BasicPie />
         </div>
-        <div className='chart-container'>
+        <div className="chart-container">
           <BasicBars />
         </div>
-        <div className='chart-container'>
+        <div className="chart-container">
           <BasicLineChart />
         </div>
       </section>
 
       <h2>My Dashboard</h2>
-      <table className='orders-table'>
+      <table className="orders-table">
         <thead>
           <tr>
             <th>Booker</th>
             <th>Stay</th>
             <th>Dates</th>
-            <th className='center'>Nights</th>
-            <th className='center'>Guests</th>
-            <th className='center'>Price / night</th>
-            <th className='center'>Total</th>
-            <th className='center'>Status</th>
-            <th className='center'>Actions</th>
+            <th className="center">Nights</th>
+            <th className="center">Guests</th>
+            <th className="center">Price / night</th>
+            <th className="center">Total</th>
+            <th className="center">Status</th>
+            <th className="center">Actions</th>
           </tr>
         </thead>
         <tbody>
           {orders.map((order) => {
-            const stayNightsNum = getNumberOfNights(order.checkIn, order.checkOut)
+            const stayNightsNum = getNumberOfNights(
+              order.checkIn,
+              order.checkOut
+            )
             const guestsNum =
               (Number(order.guests?.adults) ?? 0) +
               (Number(order.guests?.children) ?? 0)
-              
+
             return (
               <tr key={order._id}>
-                <td>{order.purchaser.fullname}</td>
+                <td>{order?.purchaser?.fullname}</td>
                 <td>{order.home.name}</td>
                 <td>
                   {`${new Date(
@@ -98,23 +100,25 @@ export function UserDetails() {
                     order.checkOut
                   ).toLocaleDateString()}`}
                 </td>
-                <td className='center'>{stayNightsNum}</td>
-                <td className='center'>{guestsNum}</td>
-                <td className='center'>${getPricePerNight(order.totalPrice, stayNightsNum)}</td>
-                <td className='center'> ${Math.round(order.totalPrice)}</td>
+                <td className="center">{stayNightsNum}</td>
+                <td className="center">{guestsNum}</td>
+                <td className="center">
+                  ${getPricePerNight(order.totalPrice, stayNightsNum)}
+                </td>
+                <td className="center"> ${Math.round(order.totalPrice)}</td>
                 <td className={`${order.status} center`}>{order.status}</td>
-                <td className='center'>
+                <td className="center">
                   {order.status === 'pending' && (
                     <>
                       <button
-                        className='approve'
-                        onClick={()=>onUpdateOrderStatus(order, 'approved')}
+                        className="approve"
+                        onClick={() => onUpdateOrderStatus(order, 'approved')}
                       >
                         Approve
                       </button>
                       <button
-                        className='reject'
-                        onClick={()=>onUpdateOrderStatus(order, 'rejected')}
+                        className="reject"
+                        onClick={() => onUpdateOrderStatus(order, 'rejected')}
                       >
                         Reject
                       </button>{' '}

f2ffd6dff11445c62e702a3bf19d33ff22de25be
Idan Bahar <bahar.idan91@gmail.com>
2025-09-27 00:13:57 +0300

    chaing vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../flexbnb - api/public',
+    outDir: '../flexbnb-api/public',
     emptyOutDir: true,
   },
   server: {
